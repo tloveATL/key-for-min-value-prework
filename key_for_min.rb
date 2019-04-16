22d41840198c25383b0d65eec0565c274b224838
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 14:41:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,8 @@
 def key_for_min_value(name_hash)
   lowest_value = nil 
   lowest_key = nil
-  name_hash.each do |key, value| 
+  name_hash.each do |key, value| if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
     
 end
