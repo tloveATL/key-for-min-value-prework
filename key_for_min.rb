df15962d518872e583e5cf1387c0ca92c6b4310e
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 14:40:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  lowest_value = nil 
+  lowest_key = nil
   name_hash.each do |key, value|
     
 end
